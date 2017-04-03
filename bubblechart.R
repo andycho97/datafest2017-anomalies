2d3dea7bb427781ba28db0e0c0ea6606515d90a1
fairbet <fredxu2015@gmail.com>
2017-04-02 22:56:33 -0400

    visualization large data

--- bubblechart.R
+++ bubblechart.R
@@ -1,5 +1,29 @@
 library(readr)
-set = read_table("data.txt")
-read.delim("data.txt")
-getwd()
-set
+library(dplyr)
+library(data.table)
+library(stringr)
+library(ggrepel)
+
+#`----------bubble plot
+us_city = read.csv("us.csv")
+
+us_city$user_location_city = us_city$city
+
+us_city_pop = merge(us_city,all)
+us_city_pop$city_income = us_city_pop$data
+
+us_city_pop$city_income = as.numeric(us_city_pop$city_income %>% str_replace_all(",", ""))
+us_city_pop$srch_destination_name = us_city_pop$srch_destination_name %>% str_replace_all(",.*","")
+# us_city_pop <- subset(us_city_pop, us_city_pop$srch_children_cnt!=0)
+
+df <- aggregate(us_city_pop[, c(5,9)], list(us_city_pop$srch_destination_name), mean)
+df2 <- aggregate(us_city_pop[, c(5,9)], list(us_city_pop$srch_destination_name), FUN = length)
+df2$cnt = df2$srch_children_cnt
+df2 <- df2[,c(1,4)]
+dfm <- merge(df,df2)
+dfm <- dfm[order(-dfm$cnt),]
+dfs <- head(dfm,15)
+
+p <- ggplot(data=dfs, aes(x = as.numeric(city_income), y = as.numeric(srch_children_cnt)))
+p + geom_point(color = 'blue', size = 3) + geom_label_repel(aes(label=as.character(Group.1)))  + theme_classic(base_size = 16) +
+  xlab("Average income (USD)") + ylab("Average number of children")
--- map.R
+++ map.R
@@ -1,24 +1,20 @@
 library(readr)
 library(dplyr)
 library(ggmap)
-library(data.table)
 
-setwd('C:/Users/Hui/Desktop/datafest')
-
-all <- read_delim('all_merged',delim ="\t")
-data <- data[,c(3,18,16)]
-data[1] <- NULL
-dest <- read_delim('dest.txt',delim ="\t")
-all <- merge(dest, data, by = "srch_destination_id")
-
-usmap <- qmap("United States", zoom = 4)
+all <- read_delim('all_merged.csv',delim =",")
+all <- all[,c(7,20,34,36,37,9)]
+all <- subset(all, all$srch_children_cnt!=0)
+# data <- data[,c(3,18,16)]
+# data[1] <- NULL
+# dest <- read_delim('dest.txt',delim ="\t")
+# all <- merge(dest, data, by = "srch_destination_id")
+# usmap <- qmap("United States", zoom = 4)
 sizedf <- all %>% filter(user_location_country =="UNITED STATES OF AMERICA") %>%
   group_by(srch_destination_name) %>% summarise(count1 = n(), lat = mean(srch_destination_latitude), lon = mean(srch_destination_longitude)) %>% 
   arrange(desc(count1)) %>% mutate(prop = count1/nrow(all))
-
-circle_scale_amt <- 1
-
+usmap <- ggmap(get_googlemap(center=c(-98.5795,39.8282), scale=2, zoom=4))
+circle_scale_amt <- 0.0005
 usmap +
 geom_point(aes(x=lon, y=lat), data=sizedf, col="orange", alpha=.4, size=sizedf$count1*circle_scale_amt) + 
 scale_size_continuous(range=range(sizedf$count1))
-
